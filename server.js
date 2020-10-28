e34451d67b03dec151bc38c9c1c29b187aa606e4
Shyaboi <ian.shane.sears@gmail.com>
2020-10-28 14:48:12 -0700

    sending audio data buffer to sever and back to client

---
+++ server.js
@@ -0,0 +1,39 @@
+// const content = require('fs').readFileSync(__dirname + '/index.html', 'utf8');
+const PORT = process.env.PORT || 8888
+const httpServer = require('http').createServer((req, res) => {
+  // serve the index.html file
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Content-Length', Buffer.byteLength(1));
+//   res.end(content);
+});
+const io = require('socket.io')(httpServer);
+io.on('connect', socket => {
+
+
+
+    socket.on('video', data => {
+        console.log(data)
+      socket.broadcast.emit('got', data)
+    });
+
+      
+      //   socket.on('message', data => {
+          //     const msg = data
+          //     console.log(msg.id, msg.msg);
+          //     chatLog.push(msg)
+          //   });
+          
+          
+          
+  });
+
+
+
+
+
+
+
+
+httpServer.listen(PORT, () => {
+  console.log('go to http://localhost:'+PORT);
+});
--- src/App.js
+++ src/App.js
@@ -1,22 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
+import socketIOClient from "socket.io-client";
+const ENDPOINT = "http://localhost:8888/";
+const socket = socketIOClient(ENDPOINT);
+
+
+const constraints = window.constraints = {
+  audio: true,
+  video: true
+};
+
 
 function App() {
+  // const audio = useRef();
+  const video = useRef();
+  const otherVideo = useRef();
+
+  useEffect(() => {
+    socket.on("video", (data) => {
+      console.log(data)
+    });
+    // socket.on("got", (data) => {
+    //   console.log(data)
+    //   var otherVid = data
+    
+    // });
+  })
+
+
+  navigator.mediaDevices.getUserMedia(constraints)
+.then((stream)=> {
+
+  var mediaRecorder = new MediaRecorder(stream);
+  mediaRecorder.onstart = function(e) {
+      this.chunks = [];
+  };
+  mediaRecorder.ondataavailable = function(e) {
+      this.chunks.push(e.data);
+      var blob = new Blob(this.chunks, { 'type' : 'audio/ogg; codecs=opus' });
+      socket.emit('video', blob);
+    };
+  //   mediaRecorder.onstop = function(e) {
+  // };
+
+  // Start recording
+  mediaRecorder.start();
+
+  // Stop recording after 5 seconds and broadcast it server
+  setInterval(() => {
+    // setTimeout(function() {
+        mediaRecorder.stop()
+        mediaRecorder.start();
+      // }, 1000);
+
+  }, 700);
+  // console.log(stream)
+  // if (video.current) {
+    // var userVideoStream = stream
+    // video.current.srcObject = stream
+    socket.on('got', function(arrayBuffer) {
+        var blob = new Blob([arrayBuffer], { 'type' : 'audio/ogg; codecs=opus' });
+        var audio = document.createElement('audio');
+        audio.src = window.URL.createObjectURL(blob);
+        audio.play();
+  });
+    console.log(stream)
+// }
+  
+})
+
+
+  // navigator.mediaDevices.getUserMedia(constraints).then(stream=>{
+
+  // });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div id="container">
+      {/* <video ref={video} id="gum-local" controls autoPlay></video>
+      <video ref={otherVideo} id="gum-local" controls autoPlay></video> */}
+
+</div>
       </header>
     </div>
   );
